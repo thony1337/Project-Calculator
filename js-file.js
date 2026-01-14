947c3f82cae0c75e0a98b042fdba57c5812867a9
thony1337 <anthonyymartins@gmail.com>
2026-01-14 16:24:48 -0300

    add: operator/numbers variables and operate functi on using operator and two numbers;

--- js-file.js
+++ js-file.js
@@ -11,3 +11,12 @@ let multiply = function(a, b) {
 let divide = function (a, b) {
     return a / b;
 }
+
+let number1 = '';
+let number2 = '';
+let operator = ''
+
+function operate(operator, number1, number2){
+  return operator(number1, number2)
+}
+;
